cee0da77c471c1568804eea4625ddce293b30a08
Kevin <40896116+kf20@users.noreply.github.com>
2021-03-25 21:35:08 -0400

    Update 2_deploy_contracts.js

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,9 @@
-const ContractProxy = artifacts.require("./ContractProxy.sol");
-const ContractLogic = artifacts.require("./ContractLogic.sol");
 const TokenA = artifacts.require("./TokenA.sol");
-
+const ContractLogic = artifacts.require("./ContractLogic.sol");
+const ContractProxy = artifacts.require("./ContractProxy.sol");
 
 module.exports = function(deployer, _network, _accounts) {
-    //deployer.deploy(ContractLogic);
-    deployer.deploy(ContractProxy, "0xE0f2aE58d3D24ADA46571F5D0370D380Edd781aC");
     deployer.deploy(TokenA);
+    deployer.deploy(ContractLogic);
+    deployer.deploy(ContractProxy);
 }
