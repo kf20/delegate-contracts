88583be414ef5aba58e6a30473fd07c3ff26e2bd
Kevin <40896116+kf20@users.noreply.github.com>
2021-04-16 11:22:57 -0400

    formatting ContractProxy.sol

--- contracts/ContractProxy.sol
+++ contracts/ContractProxy.sol
@@ -3,23 +3,19 @@ pragma solidity ^0.8.0;
 
 contract ContractProxy {
 
-    address logicContract;
+    address public logicContract;
 
     function setLogic(address _address) public {
         logicContract = _address;
     }
 
-    function getLogic() public view returns (address) {
-        return logicContract;
-    }
-
-    function transfer(address _token, address _to, uint256 _amount ) public returns (bool success) {
+    function transfer(address _token, address _to, uint256 _amount) public returns (bool success) {
         (success,) = logicContract.delegatecall(
             abi.encodeWithSignature("transfer(address,address,uint256)", _token, _to, _amount)
         );
     }
 
-    function transferFrom(address _token, address _from, address _to, uint256 _amount ) public returns (bool success) {
+    function transferFrom(address _token, address _from, address _to, uint256 _amount) public returns (bool success) {
         (success,) = logicContract.delegatecall(
             abi.encodeWithSignature("transferFrom(address,address,address,uint256)", _token, _from, _to, _amount)
         );
